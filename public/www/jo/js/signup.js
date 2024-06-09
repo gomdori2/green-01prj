b1281d98fd5e854590f1fcdc32456a202e6afe96
jowonyoung1 <jwy7254@naver.com>
2024-06-10 01:49:33 +0900

    Feat : 회원가입창 자바스크립트 작업중

---
+++ public/www/jo/js/signup.js
@@ -0,0 +1,34 @@
+//  아이디 중복 확인
+function checkUserID() {
+    const useridInput = document.getElementById('userid');
+    const username = useridInput.value;
+
+    // AJAX를 사용하여 서버에 아이디 중복 확인 요청을 보냅니다.
+    console.log('아이디 중복확인:', username);
+
+    // 서버 응답에 따라 메시지를 표시합니다.
+    const isDuplicate = false; // 서버 응답을 받아와서 중복 여부를 결정해야 합니다.
+    const usingMessage = document.querySelector('.using-message');
+    const availableMessage = document.querySelector('.available-message');
+
+    if (isDuplicate) {
+        usingMessage.style.display = 'block';
+        availableMessage.style.display = 'none';
+    } else {
+        usingMessage.style.display = 'none';
+        availableMessage.style.display = 'block';
+    }
+}
+
+//  비밀번호 일치 확인
+function checkUserPw() {
+    const userPw = document.getElementById('password');
+    const userPwConfirm = document.getElementById('confirm-password');
+    const pwErrorMessage = document.querySelector('.pw-error-message');
+
+    if (userPw.value !== userPwConfirm.value) {
+        pwErrorMessage.style.display = 'block';
+    } else {
+        pwErrorMessage.style.display = 'none';
+    }
+}
