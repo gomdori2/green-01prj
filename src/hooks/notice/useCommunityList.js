89d75e2a9876c8339e85d4a81459ee5a7ece594d
miss-gif <svx327@naver.com>
2024-06-18 15:05:08 +0900

    Test : 커스텀 훅 수정

--- src/hooks/notice/useCommunityList.js
+++ src/hooks/notice/useCommunityList.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const useCommunityList = () => {
   const [order, setOrder] = useState(3);
@@ -7,19 +8,28 @@ const useCommunityList = () => {
   const [size, setSize] = useState(15);
   const [page, setPage] = useState(1);
 
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
   useEffect(() => {
+    setIsLoading(true);
     const getCommunityList = async (order, search, keyword, size, page) => {
       try {
         const API_URL = `/api/community/list?order=${order}&search=${search}&keyword=${keyword}&size=${size}&page=${page}`;
         const response = await axios.get(API_URL);
         console.log("response", response);
+        setData(response.data);
       } catch (error) {
-        console.log(error);
+        setError(error);
+      } finally {
+        setIsLoading(false);
       }
     };
-  }, []);
+    getCommunityList();
+  }, [order, search, keyword, size, page]);
 
-  return <div>useCommunityList</div>;
+  return { order, search, keyword, size, page, data, isLoading, error };
 };
 
 export default useCommunityList;
